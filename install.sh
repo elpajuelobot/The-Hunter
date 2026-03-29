617a703a8d6a056ebe004e934726455c75b45cd5
elpajuelobot <elpajuelobot@gmail.com>
2026-03-29 14:09:10 +0200

    Initial release: The-Hunter Framework v1.0

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "[-] Iniciando preparación del entorno..."
+
+if ! command -v unzip &> /dev/null; then
+    sudo apt-get update && sudo apt-get install unzip -y
+fi
+
+if [ ! -f "data.assets" ]; then
+    echo "[!] Error: No se encuentra data.assets."
+    exit 1
+fi
+
+echo "[?] Introduce la clave para desplegar el laboratorio (password123):"
+read -s password
+
+unzip -P "$password" data.assets > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    if [ -d "src" ]; then
+        mv src/* .
+        rmdir src
+    fi
+
+    echo "[+] Recursos desplegados. Iniciando motor..."
+    chmod +x init.sh
+    sudo ./init.sh
+else
+    echo "[X] Clave incorrecta."
+    exit 1
+fi
